19235268713b8637df9c7a45f7b6a11db7152c19
Faruk <faruk.akyol@consultant.boyner.com.tr>
2024-06-13 19:43:36 +0300

    Added interface

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Lonely from "./singleton";
-
-console.log("Hello from typescript");
-
-const l1 = Lonely.getInstance();
-const l2 = Lonely.getInstance();
-
-console.log(l1 === l2); // return true
